2ab0a83dc91dfd5b1d6ea732d4b04373249df428
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-08 15:03:07 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(kids, event) {
+    let child = [];
+    for (let i = 0; i < kids.length; i++) {
+        child.push(`Thank you, ${kids[i]}, for the wonderful ${event} gift!`);
+}
+return child;
+};
+
+function countDown() {
+    let decrease = 10
+    while (decrease > -1) {
+        console.log(decrease--)};
+}
